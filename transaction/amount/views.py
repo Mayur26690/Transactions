4b92319a1ce0be4b14598a27d1ba053eaa13e86f
mayur26690 <Mayur@Niravs-MacBook-Pro.local>
2017-07-18 20:40:08 -0700

    with subtotal and budget

--- transaction/amount/views.py
+++ transaction/amount/views.py
@@ -16,6 +16,7 @@ def Datasheet(req):
     restaurent_total = models.Spent.objects.aggregate(Sum('restaurent'))
     shopping_total = models.Spent.objects.aggregate(Sum('shopping'))
     sport_total = models.Spent.objects.aggregate(Sum('sport'))
+    final_total = models.Spent.objects.all().aggregate(Sum('id'))
 
     return render(req,'amount/hotel.html',{'ans':ans, 'spent':all, 'movie_total':movie_total,
-        'hotel_total':hotel_total,'restaurent_total':restaurent_total,'shopping_total':shopping_total,'sport_total':sport_total})
+        'hotel_total':hotel_total,'restaurent_total':restaurent_total,'shopping_total':shopping_total,'sport_total':sport_total,'final_total':final_total})
